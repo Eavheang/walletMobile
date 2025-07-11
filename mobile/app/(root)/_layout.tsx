4fa977f01dfb9d5423538a954ac6dd647dd72475
Eavheang Oeng <oengeavheang@gmail.com>
2025-07-11 20:20:46 +0700

    Add Frontend

---
+++ mobile/app/(root)/_layout.tsx
@@ -0,0 +1,13 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+import { useUser } from '@clerk/clerk-expo'
+import { Redirect } from 'expo-router'
+import {Stack} from 'expo-router'
+
+export default function Layout(){
+  const {isSignedIn} = useUser()
+  if(!isSignedIn){
+    return <Redirect href={'/sign-in'} />
+  }
+  return <Stack screenOptions={{ headerShown: false }} />
+}
